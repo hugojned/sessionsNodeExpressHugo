bdf4a7b035b78eb2530acf7fdcdb33c5bf824312
Hugo Ned <jugoxnet667@gmail.com>
2024-05-22 16:29:49 -0600

    auuuu

--- app.js
+++ app.js
@@ -94,16 +94,18 @@ app.post('/login', express.urlencoded({ extended: false }), function (req, res) 
 
 app.get('/logout', function (req, res, next) {
   // logout logic
-
   // clear the user from the session object and save.
+
   // this will ensure that re-using the old session id
   // does not have a logged in user
+
   req.session.user = null
   req.session.save(function (err) {
     if (err) next(err)
 
     // regenerate the session, which is good practice to help
     // guard against forms of session fixation
+
     req.session.regenerate(function (err) {
       if (err) next(err)
       res.redirect('/')
